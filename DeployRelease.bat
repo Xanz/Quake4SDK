cf8c6f098f102368f8e981423e23c9ab59dc234a
Xanz <xanrealmz@hotmail.co.uk>
2024-01-07 20:40:48 +0000

    Cmake support

--- DeployRelease.bat
+++ DeployRelease.bat
@@ -1,5 +1,5 @@
 @echo off
-set Q4ModName=MODNAMEHERE
+set Q4ModName=xanzcoop
 set Q4InstallPath=C:\Program Files (x86)\Steam\steamapps\common\Quake 4
 REM We have to delete these otherwise your game dll will be overwritten.
 del "%Q4InstallPath%\q4base\game000.pk4"
@@ -10,6 +10,5 @@ del "%Q4InstallPath%\q4mp\game000.pk4"
 del "%Q4InstallPath%\q4mp\game100.pk4"
 del "%Q4InstallPath%\q4mp\game200.pk4"
 del "%Q4InstallPath%\q4mp\game300.pk4"
-rename ".\Builds\Win32\Release\Gamex86.dll" "gamex86.dll"
-xcopy /Y ".\Builds\Win32\Release\gamex86.dll" "%Q4InstallPath%\%Q4ModName%\"
+xcopy /Y ".\build\Release\gamex86.dll" "%Q4InstallPath%\%Q4ModName%\"
 pause
--- framework/BuildVersion.h
+++
@@ -1,3 +0,0 @@
-// that one is no longer updated with the Q4 source in SVN
-// but is used in some of the single player mishmush so I don't want to touch it
-const int BUILD_NUMBER = 1283;
